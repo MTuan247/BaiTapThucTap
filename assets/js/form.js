e4876dbe67226ca54e6ea66458442a0c6c193121
MTuan247 <mtuan2472000@gmail.com>
2021-07-05 12:50:10 +0700

    update form modal

---
+++ assets/js/form.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+
+    $('#add-employee').click(() => {
+        $('.modal').css('display','flex')
+    })
+
+    $('.info-form .close').click(() => {
+        $('.modal').css('display','none')
+    })
+
+});
